799674a8388da3c0db79c409cbf4d2e065da4bc9
Takayama2258 <47190035+Takayama2258@users.noreply.github.com>
2020-09-29 04:29:03 +0800

    Update and rename 1.py to code.py

--- code.py
+++ code.py
@@ -1,67 +1,63 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+from time import time
 
 class SimplePendulum:
-    """Simple Pendulum Class"""
+    """create a class of pendulum movement"""
     def __init__(self,
-                 init_angle=50,
-                 L=1.0,  # length of pendulum in m
-                 G=9.8,  # acceleration due to gravity, in m/s^2
+                 init_angle=45,
+                 L=1.0, 
+                 G=9.8, 
                  origin=(0,0)): 
         self.L = L
         self.G = G
         self.origin = origin
-        self.time_elapsed = 0
+        self.time = 0
         self.init_angle = init_angle * np.pi / 180.
         self.angle = self.init_angle
     
+    def step(self, t):
+      
+        self.time += t
+        self.angle = self.init_angle * np.cos(np.sqrt( self.G / self.L) * self.time)
+    
     def position(self):
-        """compute the current x,y position of the pendulum"""
+        """calculate the updating position"""
         x = np.cumsum([self.origin[0], self.L * np.sin(self.angle)])
         y = np.cumsum([self.origin[1], -self.L * np.cos(self.angle)])
         return (x, y)
 
-    def step(self, dt):
-        """execute one time step of length dt and update state"""
-        self.time_elapsed += dt
-        self.angle = self.init_angle * np.cos(np.sqrt( self.G / self.L) * self.time_elapsed)
-
-#------------------------------------------------------------
-# set up initial state and global variables
 pendulum = SimplePendulum(40)
-dt = 1./30 # 30 fps
+t = 0.01
 
-#------------------------------------------------------------
-# set up figure and animation
 fig = plt.figure()
 ax = fig.add_subplot(111, aspect='equal', autoscale_on=False, 
                      xlim=(-2, 2), ylim=(-2, 2))
 ax.grid()
 
-line, = ax.plot([], [], 'o-', lw=2, color='black')
+line, = ax.plot([], [], 'o-', lw=2, color='yellow')
 time_text = ax.text(0.02, 0.95, '', transform=ax.transAxes)
 
 def init():
-    """initialize animation"""
+    
     line.set_data([], [])
     time_text.set_text('')
     return line, time_text
 
 def animate(i):
-    """perform animation step"""
-    global pendulum, dt
-    pendulum.step(dt)
+    
+    global pendulum, t
+    pendulum.step(t)
     
     line.set_data(*pendulum.position())
     return line
 
-# choose interval based on dt and the time to animate one step
-from time import time
+
 t0 = time()
 animate(0)
 t1 = time()
-interval = 1000 * dt - (t1 - t0)
+interval = 1000 * t - (t1 - t0)
 
 ani = animation.FuncAnimation(fig,
                               animate,
